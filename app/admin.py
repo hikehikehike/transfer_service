21d0a7c3950fa3093d70c4fe620d49680068891a
Timur <timur.hike@gmail.com>
2022-12-02 15:34:52 +0200

    Create models and changed __str__

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from app.models import Car, Flight, Client
+
+admin.site.register(Car)
+admin.site.register(Flight)
+admin.site.register(Client, UserAdmin)
--- app/models.py
+++ app/models.py
@@ -3,19 +3,26 @@ from django.db import models
 
 
 class Car(models.Model):
-    capacity = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, default="Car")
+    capacity = models.IntegerField()
     comfort = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}. Capacity: {self.capacity}. Comfort: {self.comfort}"
 
 
 class Flight(models.Model):
     name = models.CharField(max_length=255)
-    departure_time = models.TimeField()
-    arrival_time = models.TimeField()
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
     cost = models.IntegerField()
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}. Travel time:{self.arrival_time - self.departure_time}"
 
 
 class Client(AbstractUser):
     name = models.CharField(max_length=255)
-    phone_number = models.IntegerField()
+    phone_number = models.IntegerField(null=True, blank=True)
     flight = models.ManyToManyField(Flight, related_name="client")
