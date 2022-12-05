9d7c540566b1fdb1d9d621037d3a352f3da61c9c
Timur <timur.hike@gmail.com>
2022-12-05 18:15:51 +0200

    Add a href in flight list

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", index, name="index"),
     path("flight/", FlightListViews.as_view(), name="flight-list"),
     path("car/", CarListViews.as_view(), name="car-list"),
-    path("flight/<int:pk>", FlightDetailViews.as_view(), name="flight-detail")
+    path("flight/<int:pk>/", FlightDetailViews.as_view(), name="flight-detail")
 ]
 
 
