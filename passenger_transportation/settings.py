31c144df75975565b2f8b7ee0c1ff393c87c9b18
Timur <timur.hike@gmail.com>
2022-12-27 12:31:11 +0200

    Add url in settings allowed hosts

--- passenger_transportation/settings.py
+++ passenger_transportation/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://southern-express.onrender.com"]
 
 
 # Application definition
