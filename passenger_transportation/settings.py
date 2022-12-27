4c05385beb495c18c84e2bf94fed111f4514b7d5
Timur <timur.hike@gmail.com>
2022-12-27 12:49:35 +0200

    Rename hosts in settings allowed hosts

--- passenger_transportation/settings.py
+++ passenger_transportation/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = ["https://southern-express.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "southern-express.onrender.com"]
 
 
 # Application definition
